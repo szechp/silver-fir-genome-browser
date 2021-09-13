64f7240dc94bbc7df55d1fb07cf7782371743f22
2894673 <setschmann@gmail.com>
2021-09-13 23:51:08 +0200

    working prototype of full-text search

--- app.R
+++ app.R
@@ -5,25 +5,56 @@ library(JBrowseR)
 #set working directory to script destination
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-##get neccesary data
+##get necessary data
 
 source("data_wrangling_scripts/read_gff.R")
+
+data_server <-
+  serve_data("D:/Masterarbeit/silver-fir-genome-browser/genome_data")
 
 options(DT.options = list(pageLength = 10))
 
 ui <- fluidPage(titlePanel("Tabsets"),
                 mainPanel(tabsetPanel(
                   type = "tabs",
-                  tabPanel("full-text search", DT::dataTableOutput("DT_annotations"), verbatimTextOutput('y11')),
+                  tabPanel(
+                    "full-text search",
+                    DT::dataTableOutput("DT_annotations"),
+                    verbatimTextOutput("select_entry"),
+                    JBrowseROutput("browserOutput")
+                  ),
                   tabPanel("BLAST-search")
-                ),))
+                ), ))
 
 server <- function(input, output, session) {
   output$DT_annotations <-
     DT::renderDataTable(annotations, selection = "single")
   
-  output$y11 = renderPrint(input$DT_annotations_rows_selected)
+  output$select_entry = renderPrint(url())
 
+  url <- reactive(paste0("http://127.0.0.1:5000/", annotations[input$DT_annotations_rows_selected,1], ".fa")
+  )
+  
+  output$browserOutput <- renderJBrowseR({JBrowseR("View",
+                                                  assembly = assembly(url()),
+                                                  tracks = tracks(track_feature("http://127.0.0.1:5000/Abal.1_1.gff.fixed.sorted_v2.gff.gz",
+                                                                                assembly(url()))),
+                                                  location = "aalba5_s00000010:168,102..180,937", #placeholder
+                                                  defaultSession = default_session(assembly(url()),
+                                                                                    c(track_feature("http://127.0.0.1:5000/Abal.1_1.gff.fixed.sorted_v2.gff.gz",
+                                                                                                    assembly(url()))))
+    )
+  })
 }
 
 shinyApp(ui, server)
+
+
+
+
+
+
+
+
+
+
