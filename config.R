526eac68fdf1c49acaaa7b57b2917e2c8c88c7ae
2894673 <setschmann@gmail.com>
2021-09-30 12:29:19 +0200

    added blast path to config

--- config.R
+++ config.R
@@ -3,3 +3,6 @@
 splitted_fastas_url <- "http://127.0.0.1:5000/splitfasta/splitted/"
 #specify the location of the annotation file (a sorted bigzipped gff file is needed)
 annotation_file_url <- "http://127.0.0.1:5000/Abal.1_1.gff.fixed.sorted_v2.gff.gz"
+
+#path to the blast executables (bin folder)
+blast_path <- "~/ncbi-blast-2.12.0+/bin/"
