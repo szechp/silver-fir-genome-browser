df57a4abc3baf57325c09d31d50fecbc30eadadf
2894673 <setschmann@gmail.com>
2021-10-14 08:47:06 +0200

    integrated BLAST search into main app

--- app.R
+++ app.R
@@ -3,6 +3,10 @@ library(data.table)
 library(DT)
 library(JBrowseR)
 library(shinythemes)
+library(XML)
+library(plyr)
+library(dplyr)
+
 
 #set working directory to script destination
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
@@ -24,7 +28,7 @@ setindex(database, Names, ID)
 #check if config.R contains localhost, create local server to serve genome data if true
 if (grepl("http://127.0.0.1:5000*", splitted_fastas_url) == T &
     grepl("http://127.0.0.1:5000*", annotation_file_url) == T) {
-  data_server <- serve_data("genome_data/")
+  try(data_server <- serve_data("genome_data/"))
 }
 
 options(DT.options = list(pageLength = 5))
@@ -36,32 +40,182 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                 navbarPage("Abies Alba Genome Browser",
                   tabPanel("full-text search",
                     DT::dataTableOutput("DT_annotations"),
-                    verbatimTextOutput("select_entry"),
-                    JBrowseROutput("browserOutput")
+                    JBrowseROutput("browserOutput_ft_search")
                   ),
-                  tabPanel("BLAST-search")
+                  tabPanel("BLAST-search",
+                    tagList(
+                  tags$head(
+                    tags$link(rel="stylesheet", type="text/css",href="style.css"),
+                    tags$script(type="text/javascript", src = "busy.js")
+                  )
+                ),
+                
+                #This block gives us all the inputs:
+                mainPanel(
+                  headerPanel('Shiny Blast!'),
+                  textAreaInput('query', 'Input sequence:', value = "", placeholder = "", width = "100%", height="200px"),
+                  selectInput("eval", "e-value:", choices=c(1,0.001,1e-4,1e-5,1e-10), width="100px"),
+                  actionButton("blast", "BLAST!")
+                ),
+                
+                #this snippet generates a progress indicator for long BLASTs
+                div(class = "busy",  
+                    p("Calculation in progress.."), 
+                    img(src="https://i.stack.imgur.com/8puiO.gif", height = 100, width = 100,align = "center")
+                ),
+                
+                #Basic results output
+                mainPanel(
+                  h4("Results"),
+                  DT::dataTableOutput("blastResults"),
+                  p("Alignment:", tableOutput("clicked") ),
+                  verbatimTextOutput("alignment"),
+                  JBrowseROutput("browserOutput_BLAST_search")
+                ))
                 ))
 
 server <- function(input, output, session) {
+
+
+  #####################################
+  ###servercode for full text search###
+  #####################################
+
   output$DT_annotations <-
     DT::renderDataTable(database, selection = "single", rownames = F)
   
   output$select_entry = renderPrint(location())
 
+  #get location and filename from selected entry and display it in JBrowse
   url <- reactive(paste0(splitted_fastas_url, database[input$DT_annotations_rows_selected,2], ".fa")
   )
   
-  location <- reactive(paste0(database[input$DT_annotations_rows_selected,2],":", database[input$DT_annotations_rows_selected,4], "..", database[input$DT_annotations_rows_selected,5]))
+  location_ft_search <- reactive(paste0(database[input$DT_annotations_rows_selected,2],":", database[input$DT_annotations_rows_selected,4], "..", database[input$DT_annotations_rows_selected,5]))
   
-  output$browserOutput <- renderJBrowseR({JBrowseR("View",
+  output$browserOutput_ft_search <- renderJBrowseR({JBrowseR("View",
                                                   assembly = assembly(url()),
                                                   tracks = tracks(track_feature(annotation_file_url,
                                                                                 assembly(url()))),
-                                                  location = location(), #placeholder
+                                                  location = location_ft_search(), #placeholder
                                                   defaultSession = default_session(assembly(url()),
                                                                                     c(track_feature(annotation_file_url,
                                                                                                     assembly(url()))))
     )
+  })
+
+
+  ##################################
+  ###server code for BLAST search###
+  ##################################
+ 
+  blastresults <- eventReactive(input$blast, {
+    
+    #gather input and set up temp file
+    query <- input$query
+    tmp <- tempfile(fileext = ".fa")
+    
+    #this makes sure the fasta is formatted properly
+    if (startsWith(query, ">")){
+      writeLines(query, tmp)
+    } else {
+      writeLines(paste0(">Query\n",query), tmp)
+    }
+    
+    #calls the blast
+    data <- system(paste0("~/ncbi-blast-2.12.0+/bin/blastn", " -query ",tmp," -db ",BLAST_db_path," -evalue ",input$eval," -outfmt 5 -max_hsps 1 -max_target_seqs 10 "), intern = T)
+    #write.table(data, file = "debug2.txt")
+    xmlParse(data)
+  }, ignoreNULL= T)
+  
+  #Parse the results...
+  parsedresults <- reactive({
+    if (is.null(blastresults())){}
+    else {
+      xmltop = xmlRoot(blastresults())
+      
+      #the first chunk is for multi-fastas
+      results <- xpathApply(blastresults(), '//Iteration',function(row){
+        query_ID <- getNodeSet(row, 'Iteration_query-def') %>% sapply(., xmlValue)
+        hit_IDs <- getNodeSet(row, 'Iteration_hits//Hit//Hit_id') %>% sapply(., xmlValue)
+        hit_to <- getNodeSet(row, 'Iteration_hits//Hit//Hit_hsps//Hsp//Hsp_hit-from') %>% sapply(., xmlValue)
+        hit_from <- getNodeSet(row, 'Iteration_hits//Hit//Hit_hsps//Hsp//Hsp_hit-to') %>% sapply(., xmlValue)
+        hit_length <- getNodeSet(row, 'Iteration_hits//Hit//Hit_len') %>% sapply(., xmlValue)
+        bitscore <- getNodeSet(row, 'Iteration_hits//Hit//Hit_hsps//Hsp//Hsp_bit-score') %>% sapply(., xmlValue)
+        eval <- getNodeSet(row, 'Iteration_hits//Hit//Hit_hsps//Hsp//Hsp_evalue') %>% sapply(., xmlValue)
+        cbind(query_ID,hit_IDs,hit_from,hit_to,hit_length,bitscore,eval)
+      })
+      #this ensures that NAs get added for no hits
+      results <-  rbind.fill(lapply(results,function(y){as.data.frame((y),stringsAsFactors=FALSE)}))
+    }
+  })
+  
+  #makes the datatable
+  output$blastResults <- renderDataTable({
+    if (is.null(blastresults())){
+    } else {
+      parsedresults()
+    }
+  }, selection="single")
+  
+  #get the alignemnt information from a clicked row
+  output$clicked <- renderTable({
+    if(is.null(input$blastResults_rows_selected)){}
+    else{
+      xmltop = xmlRoot(blastresults())
+      clicked = input$blastResults_rows_selected
+      tableout<- data.frame(parsedresults()[clicked,])
+      
+      tableout <- t(tableout)
+      names(tableout) <- c("")
+      rownames(tableout) <- c("Query ID","Hit ID" , "Hit from", "to", "Length", "Bit Score", "e-value")
+      colnames(tableout) <- NULL
+      data.frame(tableout)
+    }
+  },rownames =T,colnames =F)
+  
+  #make alignments for clicked rows
+  output$alignment <- renderText({
+    if(is.null(input$blastResults_rows_selected)){}
+    else{
+      xmltop = xmlRoot(blastresults())
+      
+      clicked = input$blastResults_rows_selected
+      
+      #loop over the xml to get the alignments
+      align <- xpathApply(blastresults(), '//Iteration',function(row){
+        top <- getNodeSet(row, 'Iteration_hits//Hit//Hit_hsps//Hsp//Hsp_qseq') %>% sapply(., xmlValue)
+        mid <- getNodeSet(row, 'Iteration_hits//Hit//Hit_hsps//Hsp//Hsp_midline') %>% sapply(., xmlValue)
+        bottom <- getNodeSet(row, 'Iteration_hits//Hit//Hit_hsps//Hsp//Hsp_hseq') %>% sapply(., xmlValue)
+        rbind(top,mid,bottom)
+      })
+      
+      #split the alignments every 40 carachters to get a "wrapped look"
+      alignx <- do.call("cbind", align)
+      splits <- strsplit(gsub("(.{40})", "\\1,", alignx[1:3,clicked]),",")
+      
+      #paste them together with returns '\n' on the breaks
+      split_out <- lapply(1:length(splits[[1]]),function(i){
+        rbind(paste0("Q-",splits[[1]][i],"\n"),paste0("M-",splits[[2]][i],"\n"),paste0("H-",splits[[3]][i],"\n"))
+      })
+      unlist(split_out)
+    }
+  })
+  
+  #displays the selected row in JBrowser
+  url_BLAST_search <- reactive(paste0(splitted_fastas_url, parsedresults()[input$blastResults_rows_selected,2], ".fa")
+  )
+  
+  location_BLAST_search <- reactive(paste0(parsedresults()[input$blastResults_rows_selected,2],":", parsedresults()[input$blastResults_rows_selected,3], "..", parsedresults()[input$blastResults_rows_selected,4]))
+  
+  output$browserOutput_BLAST_search <- renderJBrowseR({JBrowseR("View",
+                                                   assembly = assembly(url_BLAST_search()),
+                                                   tracks = tracks(track_feature(annotation_file_url,
+                                                                                  assembly(url_BLAST_search()))),
+                                                    location = location_BLAST_search(), #placeholder
+                                                    defaultSession = default_session(assembly(url_BLAST_search()),
+                                                                                     c(track_feature(annotation_file_url,
+                                                                                                     assembly(url_BLAST_search()))))
+   )
   })
 }
 
--- config.R
+++ config.R
@@ -6,3 +6,6 @@ annotation_file_url <- "http://127.0.0.1:5000/Abal.1_1.gff.fixed.sorted_v2.gff.g
 
 #path to the blast executables (bin folder)
 blast_path <- "~/ncbi-blast-2.12.0+/bin/"
+
+#specify the location of the local BLAST Database
+BLAST_db_path <- c("/media/sf_Masterarbeit/silver-fir-genome-browser/genome_data/splitfasta/Abal.1_1_filtered.1000.blast_db")
